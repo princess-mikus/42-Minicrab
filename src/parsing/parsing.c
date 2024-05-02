b8c7e87922e512dc3d275d402973061e2a863380
GoroXabi <xaberortega@gmail.com>
2024-05-02 13:58:51 +0200

    empezando otra vez la tokenizacion, infile y outfile resueltos

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: xortega <xortega@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 10:29:05 by xortega           #+#    #+#             */
-/*   Updated: 2024/05/02 12:11:35 by xortega          ###   ########.fr       */
+/*   Updated: 2024/05/02 13:58:13 by xortega          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,15 +41,6 @@ char	*search_alpha(char *str)
 }
 
 
-void	new_outfile(char *line, t_command *node)
-{
-	if (ft_strchr(line, '>'))
-	{
-		node->outfile = ft_substr(line, 
-			search_alpha(ft_strchr(line, '>')) - line,
-				 rsearch_alpha(line) - line);
-	}
-}
 void	new_arg(char *line, t_command *node)
 {
 	char *temp;
@@ -78,27 +69,49 @@ int	new_cmd(char *line, t_command *node)
 	node->command = ft_substr(line, 0, ft_strlen(line));
 	return (ft_strlen(line));
 }
-
-int	new_infile(char *line, t_command *node)
+void	new_outfile(char *line, t_command *node)
+{
+	if (ft_strchr(line, '>')) //haz una funcion que devuelva true cuando un caracter c esta en el str pero esta fuera de dobles comillas
+	{
+		if (ft_strchr(search_alpha(ft_strchr(line, '>')), ' ') && ft_strchr(line, '>')[1] == ' ') // the outfile is at the beggining and there are spaces betwen the > and the filename
+			node->outfile = ft_substr(line,
+				search_alpha(ft_strchr(line, '>')) - line,
+				ft_strchr(search_alpha(ft_strchr(line, '>')), ' ') - line - 2);
+		else if (ft_strchr(search_alpha(ft_strchr(line, '>')), ' ')) // the outfile is at the beggining and there are not spaces betwen the > and the filename
+			node->outfile = ft_substr(line,
+				ft_strchr(line, '>') - line + 1,
+				ft_strchr(search_alpha(ft_strchr(line, '>')), ' ') - line - 1);
+		else if (ft_strchr(line, '>')[1] == ' ')  // the outfile is at the end and there are spaces betwen the > and the filename
+			node->outfile = ft_substr(line,
+				search_alpha(ft_strchr(line, '>')) - line,
+				ft_strlen(search_alpha(ft_strchr(line, '>'))));	
+		else // the outfile is at the end and there are not spaces betwen the > and the filename
+			node->outfile = ft_substr(line,
+				ft_strchr(line, '>') - line + 1,
+				ft_strlen(search_alpha(ft_strchr(line, '>'))));	
+	}
+}
+void	new_infile(char *line, t_command *node)
 {
 	if (ft_strchr(line, '<')) //haz una funcion que devuelva true cuando un caracter c esta en el str pero esta fuera de dobles comillas
-	{	
-		if (!ft_strchr(line , '"')) //no hay caracter '"' : " hola ls"
-			node->infile = (ft_substr(line, search_alpha(line) - line,\
-				ft_strchr(search_alpha(line), ' ') - search_alpha(line)));
-		else
-		{
-			if (search_alpha(line) - line > ft_strchr(line , '"') - line) //si el " esta antes de cualquier caracter : "" hola" jiji"
-				node->infile = (ft_substr(line, ft_strchr(line, '"') - line,
-					ft_strchr(ft_strchr(line, '"') + 1, '"') - line));
-			else // el caracter " esta despues de cualquier otro caracter : " hola "jiji""
-				node->infile = (ft_substr(line, search_alpha(line) - line,
-					ft_strchr(search_alpha(line), ' ') - line));
-		}
-		return (search_alpha(ft_strchr(line, '<')) - line);
+	{
+		if (ft_strchr(search_alpha(ft_strchr(line, '<')), ' ') && ft_strchr(line, '<')[1] == ' ') // the infile is at the beggining and there are spaces betwen the > and the filename
+			node->infile = ft_substr(line,
+				search_alpha(ft_strchr(line, '<')) - line,
+				ft_strchr(search_alpha(ft_strchr(line, '<')), ' ') - line - 2);
+		else if (ft_strchr(search_alpha(ft_strchr(line, '<')), ' ')) // the infile is at the beggining and there are not spaces betwen the > and the filename
+			node->infile = ft_substr(line,
+				ft_strchr(line, '<') - line + 1,
+				ft_strchr(search_alpha(ft_strchr(line, '<')), ' ') - line - 1);
+		else if (ft_strchr(line, '<')[1] == ' ')  // the infile is at the end and there are spaces betwen the > and the filename
+			node->infile = ft_substr(line,
+				search_alpha(ft_strchr(line, '<')) - line,
+				ft_strlen(search_alpha(ft_strchr(line, '<'))));	
+		else // the infile is at the end and there are not spaces betwen the > and the filename
+			node->infile = ft_substr(line,
+				ft_strchr(line, '<') - line + 1,
+				ft_strlen(search_alpha(ft_strchr(line, '<'))));	
 	}
-	node->infile = NULL;
-	return (0);
 }
 
 t_command *new_command(char *line)
@@ -113,10 +126,10 @@ t_command *new_command(char *line)
 	new->arg = NULL;
 	new->outfile = NULL;
 	// < infile commmand flags > outfile
-	offset += new_infile(line, new);
-	offset += new_cmd(search_alpha(line + offset), new);
-	new_arg(search_alpha(line + offset), new);
-	new_outfile(search_alpha(line), new);
+	new_infile(line, new);
+	new_outfile(line, new);
+	new_cmd(line, new);
+	new_arg(line, new);
 	return(new);
 	//repite el modelo de funcion que devuelve el offset, que reciba line + offset y que busque con la misma logica el comando, repite con las flags y con el outfile
 	//haz una funcion que devuelva true cuando un caracter c esta en el str pero esta fuera de dobles comillas
@@ -153,9 +166,11 @@ void	parse(char *line_expanded, t_command **commands)
 
 int main(void)
 {
-	char *line1 = "outfile < ls -la > infile ";
-	//char *line2 = " \" hola \" jiji  ";
-	//char *line3 = " hola \"jiji\"";
+	char *line1 = " > outfile ls -la < infile ";
+	//char *line1 = " < infile ls -la  > outfile";
+	//char *line1 = " < infile ls -la";
+	//char *line1 = " ls -la  > outfile";
+	//char *line1 = " ls -la ";
 	t_command *command;
 	
 	command = new_command(ft_strtrim(line1, " "));
