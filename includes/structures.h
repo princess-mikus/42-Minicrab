e4a11eb3172ce934c69bd87ae598d47297f629cc
princess-mikus <kiankocasaubon01@gmail.com>
2024-05-15 06:43:05 +0200

    Small error, forgot to push structures change

--- includes/structures.h
+++ includes/structures.h
@@ -6,7 +6,7 @@
 /*   By: mikus <mikus@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 13:11:09 by fcasaubo          #+#    #+#             */
-/*   Updated: 2024/05/06 14:04:49 by mikus            ###   ########.fr       */
+/*   Updated: 2024/05/15 06:42:40 by mikus            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ typedef	struct s_command
 	char				*infile;
 	char				*command;
 	char				*arg;
+	char				*path;
 	char				*outfile;
 	struct s_command	*next;
 }						t_command;
