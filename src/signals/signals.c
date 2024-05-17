811a1ab95f624d54da88fe22da3f163fbd1c3929
princess-mikus <kiankocasaubon01@gmail.com>
2024-05-17 14:47:22 +0200

    Signals solved

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,24 +6,41 @@
 /*   By: mikus <mikus@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 00:36:45 by mikus             #+#    #+#             */
-/*   Updated: 2024/05/17 01:16:11 by mikus            ###   ########.fr       */
+/*   Updated: 2024/05/17 14:39:48 by mikus            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void 	stop_signal(int sig)
+static int g_sig;
+
+void 	get_signal(int sig)
 {	
-	if (sig == SIGINT)
+	g_sig = sig;
+}
+	
+void    signal_management(void)
+{
+	signal(SIGINT, get_signal);
+	if (g_sig == SIGINT)
 	{
     	write(2, "\n", 1);
     	rl_replace_line("", 0);
     	rl_on_new_line();
     	rl_redisplay();
 	}
+	signal(SIGQUIT, SIG_IGN);
 }
-	
-void    signal_management(void)
+
+void	signal_sender(t_command *command)
 {
-	signal(SIGINT, stop_signal);
+	signal(SIGQUIT, get_signal);
+	if (g_sig == SIGQUIT)
+	{
+		while (command)
+		{
+			kill(command->pid, SIGQUIT);
+			command = command->next;
+		}
+	}
 }
